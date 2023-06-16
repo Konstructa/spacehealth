3014607b11fd9892d794dc2e4649c3875fc3ca22
Konstructa <elenprojects7@gmail.com>
2023-06-16 15:02:29 -0300

    Boileplate

---
+++ src/components/title/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Title() {
+    return (
+        <View>
+            <Text>
+                SPACEHEALTH
+            </Text>
+        </View>
+    )
+}
